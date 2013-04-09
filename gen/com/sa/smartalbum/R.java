99b5552edf3f0d3f29716e9a8170cdb7b5ec5451
joonho1101 <joonho1101@gmail.com>
2013-04-09 18:02:53 -0400

    added string/dimension constants

--- gen/com/sa/smartalbum/R.java
+++ gen/com/sa/smartalbum/R.java
@@ -19,15 +19,21 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_vertical_spacing=0x7f040002;
+        public static final int detail_image_min_height=0x7f040003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
-        public static final int gridView1=0x7f080002;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080005;
+        public static final int caption=0x7f080002;
+        public static final int gridView1=0x7f080006;
+        public static final int image=0x7f080001;
+        public static final int location=0x7f080003;
         public static final int scrollView1=0x7f080000;
+        public static final int voice=0x7f080004;
     }
     public static final class layout {
         public static final int activity_detail=0x7f030000;
@@ -41,7 +47,12 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int location=0x7f050005;
+        public static final int sample_caption=0x7f050008;
+        public static final int share_facebook=0x7f050007;
+        public static final int take_photo=0x7f050004;
         public static final int title_activity_detail=0x7f050003;
+        public static final int voice=0x7f050006;
     }
     public static final class style {
         /** 
